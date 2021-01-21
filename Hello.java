474b0978678feca4ef0139068203e3891efcf804
ladylauraa <77622690+ladylauraa@users.noreply.github.com>
2021-01-21 22:42:51 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,8 @@
-class Hello {	public static void main(String[] args){		System.out.println("Judul");		System.out.println("Ladylauraa: Aplikasi Java 2021");		System.out.println("--- *** ---");}}
+class Hello {
+	public static void main(String[] args){
+		System.out.println("Judul");
+		System.out.println("Ladylauraa: Aplikasi Java 2021");
+		System.out.println("--- *** ---");
+    System.out.println("END");
+}
+}
