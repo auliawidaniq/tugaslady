33ec7c4051fb63aaf3b25f908c6a763f2f1373b4
ladylauraa <77622690+ladylauraa@users.noreply.github.com>
2021-01-21 22:45:49 +0700

    Fifth Commit

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
-class Hello {
+class Hello{
 	public static void main(String[] args){
 		System.out.println("Judul");
 		System.out.println("Ladylauraa: Aplikasi Java 2021");
 		System.out.println("--- *** ---");
-    System.out.println("END");
+		System.out.println("END");
 }
 }
