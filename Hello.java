74f4b9a687c302235228a09196f45d6f03625664
ladylauraa <angelauralady@gmail.com>
2021-01-21 22:41:12 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -1,1 +1,1 @@
-class Hello {public static void main(String[] args){System.out.println("Ladylauraa: Aplikasi Java 2021");}}
+class Hello {	public static void main(String[] args){		System.out.println("Judul");		System.out.println("Ladylauraa: Aplikasi Java 2021");		System.out.println("--- *** ---");}}
